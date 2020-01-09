3a11471a99f9b9acc5bbbab38241034b99004ee3
frontendmonster <frontendmonster@gmail.com>
2020-01-09 14:53:03 +0330

    feat(array): add toArray helper

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # Utils
 
 Common JavaScript utility functions
+
+## Arrays
+
+* toArray:
+Convert a value to an array
+
+```javascript
+toArray(null);      //=> []
+toArray(undefined); //=> []
+toArray('string');  //=> ['string']
+toArray(['array']); //=> ['array']
+toArray(itratable); //=> [...iteratable]
+toArray(others);    //=> [others]
+```
--- src/array.ts
+++ src/array.ts
@@ -10,6 +10,32 @@ export const forceArray = (x: any) => (Array.isArray(x) ? x : [x]);
  * @param {number} len
  * @param {number} [offset=0]
  * @param {number} [step=1]
+ * @returns {number[]}
  */
 export const range = (len: number, offset: number = 0, step: number = 1) =>
   [...Array(len).keys()].map(i => i * step + offset);
+
+/**
+ * returns array representation of a value
+ * @param {any} value
+ * @returns {any []}
+ */
+export const toArray = (value: any): any[] => {
+  if (value == null) {
+    return [];
+  }
+
+  if (Array.isArray(value)) {
+    return value;
+  }
+
+  if (typeof value === 'string') {
+    return [value];
+  }
+
+  if (typeof value[Symbol.iterator] === 'function') {
+    return [...value];
+  }
+
+  return [value];
+};

f1fe77034764891e429a210b27558333bfddb175
AmirabbasJ <amirabbasgamer2@gmail.com>
2021-10-06 00:37:45 +0330

    chore: add extra space

--- src/function.ts
+++ src/function.ts
@@ -42,7 +42,7 @@ type Predicate<T> = (a: T) => boolean;
  *  passesMin(2, [isDivisibleBy3, isDivisibleBy5, isSmallerThan25], x);
  *
  * isValidNumber(15) //returns true because its divisible by 5 and 3
- * isValidNumber(3) //returns false because its only divisible by 3
+ * isValidNumber(3)  //returns false because its only divisible by 3
  * isValidNumber(26) //returns false because its only larger than 25
  * isValidNumber(30) //returns true because its passes minimum 2 cases (being divisible by 5 and 3 and larger than 25)
  */

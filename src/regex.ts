de676db85fad1e93c036d70b53e657eac055935f
AmirabbasJ <amirabbasgamer2@gmail.com>
2021-10-05 21:52:12 +0330

    feat: add testRegex and extract regexes in to its own file

---
+++ src/regex.ts
@@ -0,0 +1,15 @@
+/**
+ * test regex without worrying about mutation on global regexes
+ */
+export const testRegex = (regex: RegExp, str: string) => {
+  const result = regex.test(str);
+  regex.lastIndex = 0;
+  return result;
+};
+
+/**
+ * escape regex chars
+ * Read more on {@link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions#escaping MDN}
+ */
+export const escapeRegExp = (s: string) =>
+  s.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');

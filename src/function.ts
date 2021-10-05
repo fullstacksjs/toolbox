af1c37078f9fe3e075e8f2ff24d65727ed0b8856
AmirabbasJ <amirabbasgamer2@gmail.com>
2021-10-05 21:52:02 +0330

    feat: add passesMin

--- src/function.ts
+++ src/function.ts
@@ -19,3 +19,20 @@ export const callAll =
  * returns the "NOT" of its argument
  */
 export const not = (b: unknown): boolean => !b;
+
+/**
+ * @template T
+ * @callback Predicate
+ * @param {T} a
+ * @returns {boolean}
+ */
+type Predicate<T> = (a: T) => boolean;
+/**
+ * @template T
+ * @param {number} min minimum predicates that should be passed
+ * @param {Predicate[]} ps list of predicates
+ * @param {T} a the value to apply the predicates
+ * @returns {boolean} true if specified minimum predicates pass the given value otherwise false
+ */
+export const passesMin = <T>(min: number, ps: Predicate<T>[], a: T) =>
+  ps.reduce((m, f) => (f(a) ? m - 1 : m), min) <= 0;

f50a52a00dc416d5af9da3c6d8f7a4957be2e3b5
AmirabbasJ <amirabbasgamer2@gmail.com>
2021-10-06 00:37:25 +0330

    fix: fix regex before testing too

--- src/regex.ts
+++ src/regex.ts
@@ -2,6 +2,7 @@
  * test regex without worrying about mutation on global regexes
  */
 export const testRegex = (regex: RegExp, str: string) => {
+  regex.lastIndex = 0;
   const result = regex.test(str);
   regex.lastIndex = 0;
   return result;

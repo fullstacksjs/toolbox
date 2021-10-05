4cd478799c5285f73298696551beff568a746a24
AmirabbasJ <amirabbasgamer2@gmail.com>
2021-10-05 22:57:06 +0330

    feat: add exampl for passesMin

--- src/function.ts
+++ src/function.ts
@@ -33,6 +33,18 @@ type Predicate<T> = (a: T) => boolean;
  * @param {Predicate[]} ps list of predicates
  * @param {T} a the value to apply the predicates
  * @returns {boolean} true if specified minimum predicates pass the given value otherwise false
+ * @example
+ * const isDivisibleBy3 = x => x % 3 === 0
+ * const isDivisibleBy5 = x => x % 5 === 0
+ * const isLargerThan25 = x => x > 25
+ *
+ * const isValidNumber = x =>
+ *  passesMin(2, [isDivisibleBy3, isDivisibleBy5, isSmallerThan25], x);
+ *
+ * isValidNumber(15) //returns true because its divisible by 5 and 3
+ * isValidNumber(3) //returns false because its only divisible by 3
+ * isValidNumber(26) //returns false because its only larger than 25
+ * isValidNumber(30) //returns true because its passes minimum 2 cases (being divisible by 5 and 3 and larger than 25)
  */
 export const passesMin = <T>(min: number, ps: Predicate<T>[], a: T) =>
   ps.reduce((m, f) => (f(a) ? m - 1 : m), min) <= 0;

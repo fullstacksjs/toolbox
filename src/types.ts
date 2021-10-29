b6f9e0adc83647fcd94a1a47ce53314b7956e2d7
Alireza Safaierad <frontendmonster@gmail.com>
2021-10-29 21:25:20 +0400

    feat(types): add CamelCase type

--- src/types.ts
+++ src/types.ts
@@ -1,1 +1,6 @@
 export type Truthy<T> = T extends '' | 0 | false | null | undefined ? never : T;
+
+export type CamelCase<S extends string> =
+  S extends `${infer P1}_${infer P2}${infer P3}`
+    ? `${Lowercase<P1>}${Uppercase<P2>}${CamelCase<P3>}`
+    : Lowercase<S>;

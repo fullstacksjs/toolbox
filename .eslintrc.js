f6d5e4d10053194511e09b52b491210b3c66fd86
Alireza Safaierad <frontendmonster@gmail.com>
2022-04-26 19:02:22 +0400

    chore: migrate to npm

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,5 @@
 module.exports = {
   root: true,
-  plugins: ['jest-formatting'],
   extends: ['@fullstacksjs', '@fullstacksjs/eslint-config/esm.js'],
   rules: {
     '@typescript-eslint/ban-types': 'off',

a60d4542663ba21b4a1c5050febaa3149ae6f3e6
Alireza Safaierad <frontendmonster@gmail.com>
2022-04-26 18:52:38 +0400

    fix(object): fix pruneNullOrEmpty type

--- src/object.ts
+++ src/object.ts
@@ -3,9 +3,7 @@ import { isNull } from './guards.js';
 /**
  * remove null and empty property from an object
  */
-export const pruneNullOrEmpty = <T extends Record<string, unknown>>(
-  obj: T,
-): T =>
+export const pruneNullOrEmpty = <T extends object>(obj: T): T =>
   Object.fromEntries(
     Object.entries(obj).filter(([_, value]) => !isNull(value) && value !== ''),
   ) as T;

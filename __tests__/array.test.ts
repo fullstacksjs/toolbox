a7225e68745e333431c286bbf4556709b3c5a7d5
Alireza Safaierad <frontendmonster@gmail.com>
2022-08-11 20:33:00 +0400

    feat(array): add isLastIndex

--- __tests__/array.test.ts
+++ __tests__/array.test.ts
@@ -7,6 +7,7 @@ import {
   ensureArray,
   getRandom,
   isEmpty,
+  isLastIndex,
   range,
   shuffle,
   toArray,
@@ -205,6 +206,26 @@ describe('array', () => {
 
     it.each(cases)('%j', ({ expected, arr }) => {
       expect(isEmpty(arr)).toBe(expected);
+    });
+  });
+
+  describe('isLastIndex', () => {
+    const cases = [
+      { arr: [], index: 0, expected: false },
+      { arr: [1], index: 0, expected: true },
+      { arr: [1, 2], index: 1, expected: true },
+      { arr: [], index: -1, expected: false },
+      { arr: [[], [], []], index: 0, expected: false },
+      { arr: [[], [], []], index: 2, expected: true },
+      { arr: [undefined], index: 0, expected: true },
+      { arr: [null], index: 0, expected: true },
+      { arr: [,], index: 0, expected: true }, // eslint-disable-line no-sparse-arrays
+      { arr: [, null], index: 1, expected: true }, // eslint-disable-line no-sparse-arrays
+      { arr: [, undefined], index: 1, expected: true }, // eslint-disable-line no-sparse-arrays
+    ];
+
+    it.each(cases)('%o', ({ expected, arr, index }) => {
+      expect(isLastIndex(arr, index)).toBe(expected);
     });
   });
 });

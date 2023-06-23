e1ee887fa30843a2f7217e1fe324cb129b617809
Mahdi Sheibak <mahdi.zoraghi.sheibak@gmail.com>
2023-06-23 12:52:38 -0400

    docs: add array documentation

---
+++ docs/next.config.js
@@ -0,0 +1,9 @@
+const withNextra = require('nextra')({
+  theme: 'nextra-theme-docs',
+  themeConfig: './theme.config.jsx',
+});
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+
+module.exports = withNextra(nextConfig);

85c4632183a91107b78b27d0c027c45333c79f55
Alireza Safaierad <frontendmonster@gmail.com>
2023-06-26 02:05:55 +0400

    refactor: extract shuffle

---
+++ src/array/shuffle.ts
@@ -0,0 +1,16 @@
+/**
+ * Return a shuffled version of an array
+ *
+ * @template T
+ * @param {T} arr
+ * @returns {T}
+ *
+ * @example
+ *
+ * shuffle([])        // []
+ * shuffle([1])       // [1]
+ * shuffle([1, 2, 3]) // Who knows?
+ */
+export function shuffle<T extends any[]>(arr: T): T {
+  return (arr.slice() as T).sort(() => (Math.random() < 0.5 ? 1 : -1));
+}

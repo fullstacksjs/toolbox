3930e86191dec86ae886574c721cd1918b4b71bc
Alireza Safaierad <frontendmonster@gmail.com>
2023-06-25 22:27:25 +0400

    docs: add range examples

--- src/array/range.ts
+++ src/array/range.ts
@@ -5,6 +5,15 @@
  * @param {number} [offset=0] The start value of range
  * @param {number} [step=1] The value to increment or decrement by
  * @returns {number[]}
+ *
+ * @example
+ *
+ * range(0);                // []
+ * range(5);                // [0,1,2,3,4]
+ * range(3, 5);             // [5,6,7]
+ * range(3, -2, 10);        // [-2, 8, 18]
+ * range(3, 2, -4);         // [2, -2, -6]
+ * range(3, -2, -1);        // [-2, -3, -4]
  */
 export function range(
   length: number,

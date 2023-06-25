685c426d7bce1476fa8cfe4ff0a67c0dd4384d0d
Alireza Safaierad <frontendmonster@gmail.com>
2023-06-25 18:42:32 +0400

    docs: add meta tags and favicon

--- docs/theme.config.jsx
+++ docs/theme.config.jsx
@@ -1,14 +1,49 @@
 import Image from 'next/image';
 
+const title = 'We Grow together';
+const description = 'We Grow together';
+const ogImage = {
+  url: 'https://github.com/fullstacksjs/toolbox/blob/main/assets/thumbnail.png',
+  alt: 'FullstacksJS - Toolbox',
+};
+
 /** @type {import('nextra-theme-docs').DocsThemeConfig} */
 const themeConfig = {
-  logo: <Image src="/logo.svg" alt="Logo" width={100} height={100} />,
+  logo: (
+    <div className="flex gap-1 items-center">
+      <Image src="/logo.svg" alt="Logo" width={60} height={60} />
+      <h1>Toolbox</h1>
+    </div>
+  ),
   project: {
     link: 'https://github.com/fullstacksjs/toolbox',
   },
+  head: (
+    <>
+      <link rel="icon" href="/favicon.ico" sizes="any" />
+      <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+      <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+      <link rel="manifest" href="/site.webmanifest" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta property="og:title" content="Nextra" />
+      <meta property="og:description" content="The next site builder" />
+    </>
+  ),
   useNextSeoProps() {
     return {
       titleTemplate: '%s - Toolbox',
+      themeColor: '#F39F47',
+      openGraph: {
+        title,
+        description,
+        images: ogImage,
+      },
+      twitter: {
+        title,
+        description,
+        images: ogImage,
+        card: 'summary_large_image',
+      },
     };
   },
   footer: {

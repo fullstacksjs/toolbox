48637c90389074b0b5e03e12b37390fbe6a5f5e5
Alireza Safaierad <frontendmonster@gmail.com>
2023-06-25 18:42:32 +0400

    docs: add meta tags and favicon

--- docs/tailwind.config.js
+++ docs/tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './theme.config.jsx',
   ],
   theme: {
     extend: {},

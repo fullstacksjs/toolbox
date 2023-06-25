12d3ef1ec235281c615f72ab8c02db011f402cd5
Alireza Safaierad <frontendmonster@gmail.com>
2023-06-25 18:43:19 +0400

    docs: add table of contents

---
+++ docs/components/TOC.tsx
@@ -0,0 +1,38 @@
+import { Card, Cards } from 'nextra-theme-docs';
+
+interface Props {
+  base: string;
+  meta: Record<string, string>;
+}
+
+export const TOC = ({ base, meta }: Props): React.JSX.Element => {
+  return (
+    <Cards>
+      {Object.entries(meta).map(([key, value]) => (
+        <Card
+          key={key}
+          icon={
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="h-6 w-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M21 7.5l-9-5.25L3 7.5m18 0l-9 5.25m9-5.25v9l-9 5.25M3 7.5l9 5.25M3 7.5v9l9 5.25m0-9v9"
+              />
+            </svg>
+          }
+          title={value}
+          href={`${base}/${key}`}
+        >
+          {' '}
+        </Card>
+      ))}
+    </Cards>
+  );
+};

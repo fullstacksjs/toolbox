dda78542e88ebe376f25371343503840358223d6
Alireza Safaierad <frontendmonster@gmail.com>
2023-06-26 03:36:38 +0400

    refactor: extract isLastIndex

--- src/array/array.ts
+++
@@ -1,7 +0,0 @@
-import { isEmpty } from './isEmpty';
-
-/**
- * is an index is the last index in an array
- */
-export const isLastIndex = (arr: unknown[], index: number): boolean =>
-  isEmpty(arr) ? false : index === arr.length - 1;

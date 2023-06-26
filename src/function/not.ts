890b7e56139ff948e2130ec75b01a8993bdaf3f5
Alireza Safaierad <frontendmonster@gmail.com>
2023-06-26 20:48:47 +0400

    refactor: extract not

---
+++ src/function/not.ts
@@ -0,0 +1,17 @@
+/**
+ * Returns the complement of its argument.
+ *
+ * @export
+ * @param {any} b
+ * @returns {boolean}
+ *
+ * @example
+ *
+ * not(true)  // false
+ * not(false) // true
+ * not(0)     // true
+ * not(1)     // false
+ */
+export function not(b: any): boolean {
+  return !b;
+}

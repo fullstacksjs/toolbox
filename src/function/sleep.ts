c49ebbc11eaa3da5577c1e07352e548341541d0d
Alireza Safaierad <frontendmonster@gmail.com>
2023-06-26 21:17:23 +0400

    refactor: extract sleep

---
+++ src/function/sleep.ts
@@ -0,0 +1,16 @@
+/**
+ * Returns a promise that resolves after a certain milliseconds.
+ *
+ * @param {number} ms timeout in milliseconds
+ * @returns {Promise<void>} a Promise that resolves after timeout
+ *
+ * @example
+ *
+ * sleep(1000).then(() => console.log("Hi")) // Logs hi after 1 second
+ * await sleep(1000); console.log("Hi")      // Logs hi after 1 second
+ */
+export function sleep(ms: number): Promise<void> {
+  return new Promise<void>(resolve => {
+    setTimeout(() => resolve(), ms);
+  });
+}

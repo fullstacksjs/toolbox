5a43d85709c7df7b62d48de5a5c3f5d14a565804
Alireza Safaierad <frontendmonster@gmail.com>
2023-06-28 13:20:16 +0400

    refactor: refactor string to function declaration

--- src/string/string.ts
+++ src/string/string.ts
@@ -1,4 +1,4 @@
-import { isWordOrWords } from '../internals/string.js';
+import { isWordOrWords } from '../internals/isWordOrWords.js';
 import { hasInvalidCasing, tokenize } from '../internals/tokenize.js';
 import type { Nullish, Sensitivity } from '../types/types.js';
 
@@ -7,60 +7,69 @@ export const toSpaceCase = tokenize;
 /**
  * converts crlf to lf
  */
-export const crlfToLf = (str: string): string => str.replace(/\r\n/g, '\n');
+export function crlfToLf(str: string): string {
+  return str.replace(/\r\n/g, '\n');
+}
 
 /**
  * uppercase first letter + lowercase rest
  */
-export const toCapitalCase = (str: string): string =>
-  str.charAt(0).toUpperCase() + str.substring(1).toLowerCase();
+export function toCapitalCase(str: string): string {
+  return str.charAt(0).toUpperCase() + str.substring(1).toLowerCase();
+}
 
 /**
  * convert any casing to camelCase
  */
-export const toCamelCase = (str: string) =>
-  hasInvalidCasing(str)
+export function toCamelCase(str: string): string {
+  return hasInvalidCasing(str)
     ? str
     : toSpaceCase(str).replace(/\s([a-z])/g, (_, letter) =>
         letter.toUpperCase(),
       );
+}
 
 /**
  * convert any casing to snake_case
  */
-export const toSnakeCase = (str: string) =>
-  hasInvalidCasing(str)
+export function toSnakeCase(str: string) {
+  return hasInvalidCasing(str)
     ? str
     : toSpaceCase(str).replace(
         /(\s[a-z])/g,
         (_, letter: string) => `_${letter.slice(1)}`,
       );
+}
 
 /**
  * convert any casing to kebab-case
  */
-export const toKebabCase = (str: string) =>
-  hasInvalidCasing(str)
+export function toKebabCase(str: string) {
+  return hasInvalidCasing(str)
     ? str
     : toSpaceCase(str).replace(
         /(\s[a-z])/g,
         (_, letter: string) => `-${letter.slice(1)}`,
       );
+}
 
 /**
  * convert any casing to PascalCase
  */
-export const toPascalCase = (str: string) =>
-  hasInvalidCasing(str)
+export function toPascalCase(str: string) {
+  return hasInvalidCasing(str)
     ? str
     : toCamelCase(str).replace(/^./, m => m.toUpperCase());
+}
 
 /**
  * check if value is null, undefined or empty string or array
  */
-export const isNullOrEmpty = <T extends any[] | string>(
+export function isNullOrEmpty<T extends any[] | string>(
   x: Nullish | T | '' | [],
-): x is Nullish | (T extends any[] ? [] : '') => x == null || x.length === 0;
+): x is Nullish | (T extends any[] ? [] : '') {
+  return x == null || x.length === 0;
+}
 
 /**
  * return initial chars of words
@@ -69,35 +78,39 @@ export const isNullOrEmpty = <T extends any[] | string>(
  * @returns {string} initials chars of a words || fallback
  * @example getInitials('frontend monsters'); //-> FM
  */
-export const getInitials = (name: string, fallback: string = '?'): string =>
-  typeof name === 'string' && isWordOrWords(name.trim())
+export function getInitials(name: string, fallback: string = '?'): string {
+  return typeof name === 'string' && isWordOrWords(name.trim())
     ? name
         .replace(/\s+/g, ' ')
         .split(' ')
         .map(v => (isNullOrEmpty(v) ? '' : v.slice(0, 1).toUpperCase()))
         .join('')
     : fallback;
+}
 
 /**
  * removing trailing slashes from a string
  */
-export const removeTrailingSlashes = (str: string): string =>
-  str.replace(/\/+$/g, '');
+export function removeTrailingSlashes(str: string): string {
+  return str.replace(/\/+$/g, '');
+}
 
 /**
  * removing leading slashes from a string
  */
-export const removeLeadingSlashes = (s: string) => s.replace(/^\/+/, '');
+export function removeLeadingSlashes(s: string): string {
+  return s.replace(/^\/+/, '');
+}
 
 /**
  * join path parts with / and without trailing and leading slashes
  */
-export const joinPath = (...paths: string[]): string => {
+export function joinPath(...paths: string[]): string {
   const result = paths.map(el =>
     removeLeadingSlashes(removeTrailingSlashes(el)),
   );
   return result.join('/');
-};
+}
 
 /**
  * Compare two path without trailing and leading slashes
@@ -105,14 +118,14 @@ export const joinPath = (...paths: string[]): string => {
  * @param {string} path2: second path
  * @return {boolean} is they are equal
  */
-export const comparePaths = (
+export function comparePaths(
   path1: string,
   path2: string,
   { sensitivity = 'accent' }: { sensitivity?: Sensitivity } = {},
-): number => {
+): number {
   const normalPath1 = removeLeadingSlashes(removeTrailingSlashes(path1));
   const normalPath2 = removeLeadingSlashes(removeTrailingSlashes(path2));
   return normalPath1.localeCompare(normalPath2, undefined, {
     sensitivity,
   });
-};
+}

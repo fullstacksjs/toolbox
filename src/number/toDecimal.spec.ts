092c1553debe265e92fa1ce709746ed3fa130100
Alireza Safaierad <frontendmonster@gmail.com>
2023-06-29 00:41:30 +0400

    feat(number): add toDecimal function
    toInteger renamed to toDecimal
    
    BREAKING CHANGE: toDecimal removed

---
+++ src/number/toDecimal.spec.ts
@@ -0,0 +1,24 @@
+import { describe, expect, it } from 'vitest';
+import { toDecimal } from './toDecimal';
+
+describe('toDecimal', () => {
+  it('should convert the string to an integer', () => {
+    expect(toDecimal('100')).toBe(100);
+  });
+
+  it('should truncate decimals', () => {
+    expect(toDecimal('1.42')).toBe(1);
+  });
+
+  it('the default fallback should be NaN', () => {
+    expect(toDecimal('S')).toBeNaN();
+  });
+
+  it('should return undefined if input is not a number and fallback is undefined', () => {
+    expect(toDecimal('S', 10)).toBe(10);
+  });
+
+  it('should return undefined if input is not a number and fallback is undefined', () => {
+    expect(toDecimal('S', undefined)).toBe(NaN);
+  });
+});

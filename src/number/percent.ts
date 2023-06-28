7c2577184dd0af749ec5a8bd3b4c4d35f0082273
Alireza Safaierad <frontendmonster@gmail.com>
2023-06-29 01:03:45 +0400

    feat(percent): update percent signature
    make percent function curry-able
    
    BREAKING CHANGE: input signature changed

---
+++ src/number/percent.ts
@@ -0,0 +1,23 @@
+import { clamp } from './clamp';
+import { divide } from './divide';
+
+/**
+ * Returns percentage value of a number from a maximum number
+ *
+ * @param {number} value value
+ * @param {number} max max
+ * @returns {number} percentage of the value
+ *
+ * @example
+ *
+ * clamp(0, 10)   // 0
+ * clamp(1, 10)   // 10
+ * clamp(-1, 10)  // 0
+ * clamp(20, 10)  // 100
+ * clamp(NaN, 10) // 0
+ * clamp(10, NaN) // 0
+ * clamp(1, 3)    // 33.33333333333333
+ */
+export function percent(value: number, max: number): number {
+  return clamp(divide(value, max, 0), 0, 1) * 100;
+}

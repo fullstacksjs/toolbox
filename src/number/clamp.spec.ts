83b48e76ae4f3434594ab6e84a84323c6d21197b
Alireza Safaierad <frontendmonster@gmail.com>
2023-06-29 00:27:27 +0400

    feat(number): change clamp signature
    BREAKING CHANGE: change function signature to be curry-able

---
+++ src/number/clamp.spec.ts
@@ -0,0 +1,16 @@
+import { describe, expect, it } from 'vitest';
+import { clamp } from './clamp';
+
+describe('clamp', () => {
+  it('should clamp if value less than min', () => {
+    expect(clamp(-10, 0, 100)).toBe(0);
+  });
+
+  it('should clamp if value more than max', () => {
+    expect(clamp(101, 0, 100)).toBe(100);
+  });
+
+  it("should return the value if it's within range", () => {
+    expect(clamp(5, 0, 100)).toBe(5);
+  });
+});

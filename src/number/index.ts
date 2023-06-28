3e12f92d24c581eb9028f3d03ef7982585457411
Alireza Safaierad <frontendmonster@gmail.com>
2023-06-29 01:11:00 +0400

    feat(number): change isInRange signature
    make isInRange curry-able
    
    BREAKING CHANGE: isInRange signature changed

---
+++ src/number/index.ts
@@ -0,0 +1,6 @@
+export { clamp } from './clamp';
+export { randomInt } from './randomInt';
+export { divide } from './divide';
+export { isInRange } from './isInRange';
+export { percent } from './percent';
+export { toDecimal } from './toDecimal';
---
+++ src/number/isInRange.spec.ts
@@ -0,0 +1,25 @@
+import { describe, expect, it } from 'vitest';
+
+import { isInRange } from './isInRange';
+
+describe('isInRange', () => {
+  it('should return false when value is greater than max', () => {
+    expect(isInRange(51, 0, 50)).toBe(false);
+  });
+
+  it('should return false when value is less than min', () => {
+    expect(isInRange(-1, 0, 100)).toBe(true);
+  });
+
+  it('should return true for max', () => {
+    expect(isInRange(100, 0, 100)).toBe(true);
+  });
+
+  it('should return true for min', () => {
+    expect(isInRange(0, 0, 100)).toBe(true);
+  });
+
+  it('should return true for value between min and max', () => {
+    expect(isInRange(50, 0, 100)).toBe(true);
+  });
+});

8a1df261894e5cc8560e5c96a6d79014a1d9283a
Alireza Safaierad <frontendmonster@gmail.com>
2023-06-29 00:25:52 +0400

    feat(number): rename safeDivide to divide
    BREAKING CHANGE: safeDivide is removed

---
+++ src/number/divide.ts
@@ -0,0 +1,23 @@
+import { fallbackNumber } from '../values';
+
+/**
+ * Divides two numbers returns fallback if result is not a finite number
+ *
+ * @param {number} dividend dividend
+ * @param {number} divisor divisor
+ * @param {number} [fallback=NaN] fallback when the result is not finite
+ * @returns {number} division result or fallback
+ *
+ * @example
+ *
+ * divide(4, 2)     // 2
+ * divide(4, 0, 42) // 42
+ * divide(4, 0)     // NaN
+ */
+export function divide(
+  dividend: number,
+  divisor: number,
+  fallback: number = NaN,
+): number {
+  return fallbackNumber(dividend / divisor, fallback);
+}

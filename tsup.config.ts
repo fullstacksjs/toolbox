f19094ca4111cc349c8b66d3d14e9ef71765c253
Alireza Safaierad <frontendmonster@gmail.com>
2023-07-03 00:10:31 +0400

    build: migrate to tsup

---
+++ tsup.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+  entry: ['src/index.ts'],
+  splitting: true,
+  sourcemap: true,
+  treeshake: true,
+  minify: true,
+  legacyOutput: true,
+  format: ['cjs', 'esm', 'iife'],
+  outDir: './lib/',
+});

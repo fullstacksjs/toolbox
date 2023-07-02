6d5c6c67f884a0c3775971a7f2190473ed0a1eb8
Alireza Safaierad <frontendmonster@gmail.com>
2023-07-02 20:00:03 +0400

    feat(node): change isDev and isProd to function
    to have lazy evaluation of resolving env they are now functions
    
    BREAKING CHANGE: all other env related functions has removed and isDev and isProd is now under node
    category

---
+++ src/node/isDev.ts
@@ -0,0 +1,19 @@
+/**
+ * Check whether NodeJS Env is development.
+ *
+ * @returns {boolean} is NODE_ENV === 'development'
+ *
+ * @example
+ *
+ * NODE_ENV=development node
+ * isDev() // true
+ *
+ * NODE_ENV=production node
+ * isDev() // false
+ *
+ * NODE_ENV="" node
+ * isDev() // false
+ */
+export function isDev(): boolean {
+  return process.env['NODE_ENV'] === 'development';
+}

8ed8f56ef83dbc251872ddaf8424d08923a7f787
Alireza Safaierad <frontendmonster@gmail.com>
2023-07-03 20:26:03 +0400

    feat(error): move required to assertNotNull
    rename required to assertNotNull and improve types
    
    BREAKING CHANGE: required is removed use error/assertNotNull instead

---
+++ src/error/assertNotNull.spec.ts
@@ -0,0 +1,16 @@
+import { describe, expect, it } from 'vitest';
+import { assertNotNull } from './assertNotNull.ts';
+
+describe('assertNotNull', () => {
+  it('should throw error when condition is null', () => {
+    expect(() => assertNotNull(null, 'WTF')).toThrow('WTF');
+  });
+
+  it('should throw error when condition is undefined', () => {
+    expect(() => assertNotNull(undefined, 'WTF')).toThrow('WTF');
+  });
+
+  it('should not throw error when condition is not null', () => {
+    expect(() => assertNotNull(0)).not.toThrow();
+  });
+});
---
+++ src/error/assertNotNull.ts
@@ -0,0 +1,21 @@
+import type { Nullable } from '../types/types.js';
+
+/**
+ * Asserts a value is not null or undefined
+ *
+ * @param {Nullable<T>} value any value
+ * @param {string} [error='Value is required'] error message when the value is not null
+ * @returns {undefined}
+ *
+ * @example
+ *
+ * assertNotNull(null, 'WTF')      // Throw 'WTF'
+ * assertNotNull(undefined, 'WTF') // Throw 'WTF'
+ * assertNotNull(true)             // noop
+ */
+export function assertNotNull<T>(
+  value: Nullable<T>,
+  error: string = 'Value is required',
+): asserts value is NonNullable<T> {
+  if (value == null) throw Error(error);
+}

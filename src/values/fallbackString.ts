c61456852fd0127dccbc68e2048dc7acb3962a55
Alireza Safaierad <frontendmonster@gmail.com>
2023-07-03 20:16:59 +0400

    feat(string): move fallbackString to string scope
    rename fallbackString to fallback and improve types
    
    BREAKING CHANGE: fallbackString is removed use string/fallback instead

--- src/values/fallbackString.ts
+++
@@ -1,8 +0,0 @@
-import type { Nullable } from '../types/types.ts';
-
-export function fallbackString<T = string>(
-  value: Nullable<string>,
-  defaultValue: T,
-): T | string {
-  return value == null || value === '' ? defaultValue : value;
-}

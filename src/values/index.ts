6c32ea7e374fe7fac43a7f5c46f2a3d7161aabb8
Alireza Safaierad <frontendmonster@gmail.com>
2023-07-03 20:26:03 +0400

    feat(error): move required to assertNotNull
    rename required to assertNotNull and improve types
    
    BREAKING CHANGE: required is removed use error/assertNotNull instead

--- src/values/index.ts
+++
@@ -1,4 +0,0 @@
-export { fallback } from './fallback.ts';
-export { fallbackNumber } from './fallbackNumber.ts';
-export { required } from './required.ts';
-export { fallbackString } from './fallbackString.ts';
--- src/values/required.ts
+++
@@ -1,9 +0,0 @@
-import type { Nullable } from '../types/types.ts';
-
-export function required<T>(
-  value: Nullable<T>,
-  name: string = 'value',
-): NonNullable<T> {
-  if (value == null) throw Error(`${name} is required`);
-  return value as NonNullable<T>;
-}

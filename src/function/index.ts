3e974cc785bfa8dfca14c25a3e4ffe78af36cfb4
Keivan-sf <keyvan0082@gmail.com>
2023-08-07 18:25:54 +0330

    chore: export debounce function

--- src/function/index.ts
+++ src/function/index.ts
@@ -2,3 +2,4 @@ export { sleep } from './sleep.ts';
 export { not } from './not.ts';
 export { noop } from './noop.ts';
 export { callAll } from './callAll.ts';
+export { debounce } from './debounce.ts';

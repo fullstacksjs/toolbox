9287f91b2dad78562ec3334d4dafa33a5ba67cf6
Keivan-sf <keyvan0082@gmail.com>
2023-08-08 21:31:22 +0330

    feat: change debounce function signature

--- src/function/debounce.ts
+++ src/function/debounce.ts
@@ -1,31 +1,15 @@
 /**
  * Return a function which will only call {@link cb} when a delay is passed after its last call
  * // will log 5 after 1 second
+ * @param {Object} options debounce options
  * @param {Function} cb function to be executed after the delay
- * @param {number} delay delay in milliseconds
- * @param {boolean} immediate if *true*, will execute {@link cb} immediately
- * if it hasn't been called in the last {@link delay} milliseconds. Default is *false*
- *
- * #### Example
- * ```ts
- * function test(id: number) {
- *    console.log(id);
- * }
- * const debounced = debounce(test, 1000, true);
- * debounced(1); // immediately logs 1
- * debounced(2);
- * debounced(3);
- * debounced(4);
- * debounced(5); // will log 5 after 1 second
- * ```
- *
  * @returns {Function} a wrapper function
  *
  * @example
  * function test(id: number) {
  *    console.log(id);
  * }
- * const debounced = debounce(test, 1000);
+ * const debounced = debounce({ delay: 1000 } , test);
  * debounced(1);
  * debounced(2);
  * debounced(3);
@@ -33,21 +17,23 @@
  * debounced(5);
  */
 export function debounce<F extends (...args: any[]) => any>(
+  options: {
+    delay: number;
+    immediate?: boolean;
+  },
   cb: F,
-  delay: number,
-  immediate: boolean = false,
 ): (...args: Parameters<F>) => void {
   let timeout: ReturnType<typeof setTimeout> | null;
 
   return (...args: Parameters<F>) => {
     if (timeout) clearTimeout(timeout);
-    const immediateCall = immediate && !timeout;
+    const immediateCall = options.immediate && !timeout;
 
     if (immediateCall) cb(...args);
 
     timeout = setTimeout(() => {
       timeout = null;
       if (!immediateCall) cb(...args);
-    }, delay);
+    }, options.delay);
   };
 }

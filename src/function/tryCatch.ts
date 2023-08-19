ce4a5985c4857e844ee94cd0a5193f3ab4f9427c
Keivan-sf <keyvan0082@gmail.com>
2023-08-19 18:46:51 +0330

    chore: add `tryCatch` and `asyncTryCatch` descriptions

--- src/function/tryCatch.ts
+++ src/function/tryCatch.ts
@@ -1,3 +1,21 @@
+/**
+ * Calls {@link fn} and returns its return value if there are no errors; but if there are any, calls
+ * {@link onError} and returns its return value
+ * @param {Function} fn To be called and have its return value returned if there are no errors
+ * @param {Function} onError To be called when there is an error and have its return value returned
+ * @returns {any} on of the passed functions return value
+ *
+ * @example
+ * const execute = () => {
+ *  throw 'Unknown Error';
+ * };
+ *
+ * const handler = (err: unknown) => {
+ *  return 'Parsed Error';
+ * };
+ *
+ * tryCatch(execute, handler); // 'Parsed Error'
+ */
 export function tryCatch<T, E>(
   fn: (...args: any[]) => T,
   onError: (err: unknown) => E,

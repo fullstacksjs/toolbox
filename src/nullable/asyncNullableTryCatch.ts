eecc5ca178e6346100cc1a43e336f09ecbd59109
Keivan-sf <keyvan0082@gmail.com>
2023-08-19 19:01:49 +0330

    chore: add nullable try catch function descriptions

--- src/nullable/asyncNullableTryCatch.ts
+++ src/nullable/asyncNullableTryCatch.ts
@@ -1,3 +1,14 @@
+/**
+ * Asynchronously calls {@link fn} and returns its return value if there are no errors; but if there are any, returns
+ * null
+ * @param {Function} fn To be called and have its return value returned if there are no errors
+ * @returns {any} either the return value of {@link fn} or null
+ *
+ * @example
+ * const execute = () => Promise.reject("Unknown Error");
+ *
+ * await asyncNullableTryCatch(execute); // null
+ */
 export async function asyncNullableTryCatch<T>(
   fn: (...args: any[]) => Promise<T>,
 ): Promise<T | null> {
--- src/nullable/nullableTryCatch.ts
+++ src/nullable/nullableTryCatch.ts
@@ -1,3 +1,16 @@
+/**
+ * Calls {@link fn} and returns its return value if there are no errors; but if there are any, returns
+ * null
+ * @param {Function} fn To be called and have its return value returned if there are no errors
+ * @returns {any} either the return value of {@link fn} or null
+ *
+ * @example
+ * const execute = () => {
+ *  throw 'Unknown Error';
+ * };
+ *
+ * nullableTryCatch(execute); // null
+ */
 export function nullableTryCatch<T>(fn: (...args: any[]) => T): T | null {
   try {
     return fn();

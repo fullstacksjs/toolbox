c67d86b9eedb325a90da293ce765d4696433340d
abxlfazl <abxlfazlkhxrshidi@gmail.com>
2024-01-07 13:47:17 +0330

    test: add tests for getTypeOf function

---
+++ src/function/getTypeOf.spec.ts
@@ -0,0 +1,33 @@
+import { getTypeOf } from './getTypeOf';
+
+describe('getTypeOf', () => {
+  const cases = [
+    {
+      value: class Foo {
+        a = 1;
+      },
+      expected: 'function',
+    },
+    { value: [], expected: 'array' },
+    { value: 1, expected: 'number' },
+    { value: {}, expected: 'object' },
+    { value: null, expected: 'null' },
+    { value: 10n, expected: 'bigint' },
+    { value: 'foo', expected: 'string' },
+    { value: true, expected: 'boolean' },
+    { value: new Map(), expected: 'map' },
+    { value: new Set(), expected: 'set' },
+    { value: new Date(), expected: 'date' },
+    { value: new Error(), expected: 'error' },
+    { value: () => true, expected: 'function' },
+    { value: undefined, expected: 'undefined' },
+    { value: Symbol('bar'), expected: 'symbol' },
+    { value: new RegExp('foo'), expected: 'regexp' },
+    { value: Promise.resolve(true), expected: 'promise' },
+  ];
+
+  it.each(cases)(
+    'the typeof $value should be $expected',
+    ({ value, expected }) => expect(getTypeOf(value)).toBe(expected),
+  );
+});

c9e033392b55dc1424ee6d08b3ea8fe031b80694
abxlfazl <abxlfazlkhxrshidi@gmail.com>
2024-01-11 17:27:08 +0330

    feat: implement isMap guard

---
+++ src/guards/isMap.ts
@@ -0,0 +1,30 @@
+import { getTypeOf } from '../types';
+
+/**
+ * Check whether the given value is a Map or not.
+ *
+ * @param {unknown} x any value.
+ * @return {boolean} true if the value is a Map.
+ *
+ * @example
+ *
+ * isMap(new Map([["a", 1]]))   // true
+ * isMap(new Map())             // true
+ * isMap(new Set([1, 2]))       // false
+ * isMap(new Set())             // false
+ * isMap({a: 1})                // false
+ * isMap({})                    // false
+ * isMap([])                    // false
+ * isMap([1, 2, 3])             // false
+ * isMap(Symbol("bar"))         // false
+ * isMap("")                    // false
+ * isMap("foo")                 // false
+ * isMap(1)                     // false
+ * isMap(NaN)                   // false
+ * isMap(true)                  // false
+ * isMap(null)                  // false
+ * isMap(undefined)             // false
+ */
+export function isMap(x: unknown): x is Map<unknown, unknown> {
+  return getTypeOf(x) === 'map';
+}

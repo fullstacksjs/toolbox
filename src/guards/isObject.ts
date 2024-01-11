edce0bf260417c79f03cc504324413fe809e1559
abxlfazl <abxlfazlkhxrshidi@gmail.com>
2024-01-11 17:17:36 +0330

    fix: add strong check for isObject guard

--- src/guards/isObject.ts
+++ src/guards/isObject.ts
@@ -1,3 +1,6 @@
+import type { ObjectType } from '../types/types';
+import { getTypeOf } from '../types';
+
 /**
  * Check whether the given value is an object
  *
@@ -6,22 +9,23 @@
  *
  * @example
  *
- * isObject('')            // false
- * isObject('hello world') // false
- * isObject(null)          // false
- * isObject(true)          // false
- * isObject(undefined)     // false
- * isObject(NaN)           // false
- * isObject(0)             // false
- * isObject(isObject)      // false
- * isObject(false)         // false
- * isObject([])            // false
- * isObject([2])           // false
- * isObject({})            // true
- * isObject({ a: 2 })      // true
- * isObject({ 2: 'a' })    // true
+ * isObject('')                   // false
+ * isObject('hello world')        // false
+ * isObject(null)                 // false
+ * isObject(true)                 // false
+ * isObject(undefined)            // false
+ * isObject(NaN)                  // false
+ * isObject(0)                    // false
+ * isObject(isObject)             // false
+ * isObject(false)                // false
+ * isObject([])                   // false
+ * isObject([2])                  // false
+ * isObject(new Map())            // false
+ * isObject(new Set())            // false
+ * isObject(new RegExp('foo'))    // false
+ * isObject({})                   // true
+ * isObject({ a: 2 })             // true
+ * isObject({ 2: 'a' })           // true
  */
-export const isObject = (
-  x: unknown,
-): x is Record<number | string | symbol, any> =>
-  typeof x === 'object' && !Array.isArray(x) && x !== null;
+export const isObject = (x: unknown): x is ObjectType =>
+  getTypeOf(x) === 'object' && x !== null;

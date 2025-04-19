0503b48484151260f90c35c40360159435bf7b4c
Alireza Safaierad <frontendmonster@gmail.com>
2025-04-20 00:05:17 +0200

    fix: move isPositive and isNegative to numbers

--- src/number/index.ts
+++ src/number/index.ts
@@ -2,6 +2,8 @@ export { clamp } from './clamp.ts';
 export { divide } from './divide.ts';
 export { fallbackNumber } from './fallbackNumber.ts';
 export { isInRange } from './isInRange.ts';
+export { isNegative } from './isNegative.ts';
+export { isPositive } from './isPositive.ts';
 export { percent } from './percent.ts';
 export { randomInt } from './randomInt.ts';
 export { toDecimal } from './toDecimal.ts';

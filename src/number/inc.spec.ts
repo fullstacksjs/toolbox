26e470dec8ecfad08b984698977f819e36c1d327
erfan-goodarzi <devgoodarzi@gmail.com>
2025-05-03 17:25:55 +0330

    fix: remove step form inc & dec functions

--- src/number/inc.spec.ts
+++ src/number/inc.spec.ts
@@ -1,17 +1,8 @@
 import { inc } from './inc';
 
 describe('inc', () => {
-  it('should increment by 1 when no step is provided', () => {
+  it('should increment by 1', () => {
     expect(inc(0)).toBe(1);
     expect(inc(5)).toBe(6);
-  });
-
-  it('should increment by the given step', () => {
-    expect(inc(0, 2)).toBe(2);
-    expect(inc(5, 3)).toBe(8);
-  });
-
-  it('should handle negative steps correctly', () => {
-    expect(inc(5, -2)).toBe(3);
   });
 });

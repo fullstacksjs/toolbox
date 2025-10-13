e0c5efd1b77fabfc7204571695b901848ba3afa6
Taha. Dostifam <mr.tahadostifam@gmail.com>
2025-10-13 21:18:12 +0330

    refactor: used hand made generics instead of pure generics of ts to make  and

--- src/types/pipe.spec.ts
+++ src/types/pipe.spec.ts
@@ -1,12 +1,91 @@
-import { pipe } from './pipe';
+import { pipe, pipeWith } from './pipe';
+
+const zeroParamGetNumber = () => 1;
+const singleParamFnAdd1 = (n: number) => n + 1;
+const singleParamFnTimes2 = (n: number) => n * 2;
+const multipleParamFnDifference = (a: number, b: number) => a - b;
 
 describe('pipe', () => {
-  it('applies functions sequentially', () => {
-    const result = pipe(
-      2,
-      (x: number) => x + 2,
-      (x: number) => x * 3,
-    );
-    expect(result).toBe(12); // (2+2)*3
+  it('works when first function has 0 params', () => {
+    expect(pipe(zeroParamGetNumber, singleParamFnAdd1)()).toBe(2);
+  });
+
+  it('works when first function has single param', () => {
+    expect(pipe(singleParamFnAdd1)(1)).toBe(2);
+    expect(pipe(singleParamFnAdd1, singleParamFnTimes2)(1)).toBe(4);
+    expect(
+      pipe(singleParamFnAdd1, singleParamFnTimes2, singleParamFnAdd1)(1),
+    ).toBe(5);
+    expect(
+      pipe(
+        singleParamFnAdd1,
+        singleParamFnTimes2,
+        singleParamFnAdd1,
+        singleParamFnTimes2,
+      )(1),
+    ).toBe(10);
+    expect(
+      pipe(
+        singleParamFnAdd1,
+        singleParamFnTimes2,
+        singleParamFnAdd1,
+        singleParamFnTimes2,
+        singleParamFnAdd1,
+      )(1),
+    ).toBe(11);
+    expect(
+      pipe(
+        singleParamFnAdd1,
+        singleParamFnTimes2,
+        singleParamFnAdd1,
+        singleParamFnTimes2,
+        singleParamFnAdd1,
+        singleParamFnTimes2,
+      )(1),
+    ).toBe(22);
+  });
+
+  it('works when first function has multiple params', () => {
+    expect(pipe(multipleParamFnDifference, singleParamFnAdd1)(5, 4)).toBe(2);
+  });
+});
+
+describe('pipeWith', () => {
+  it('works', () => {
+    expect(pipeWith(1, singleParamFnAdd1)).toBe(2);
+    expect(pipeWith(1, singleParamFnAdd1, singleParamFnTimes2)).toBe(4);
+    expect(
+      pipeWith(1, singleParamFnAdd1, singleParamFnTimes2, singleParamFnAdd1),
+    ).toBe(5);
+    expect(
+      pipeWith(
+        1,
+        singleParamFnAdd1,
+        singleParamFnTimes2,
+        singleParamFnAdd1,
+        singleParamFnTimes2,
+      ),
+    ).toBe(10);
+    expect(
+      pipeWith(
+        1,
+        singleParamFnAdd1,
+        singleParamFnTimes2,
+        singleParamFnAdd1,
+        singleParamFnTimes2,
+        singleParamFnAdd1,
+      ),
+    ).toBe(11);
+    expect(
+      pipeWith(
+        1,
+        singleParamFnAdd1,
+        singleParamFnTimes2,
+        singleParamFnAdd1,
+        singleParamFnTimes2,
+        singleParamFnAdd1,
+        singleParamFnTimes2,
+      ),
+    ).toBe(22);
   });
 });
--- src/types/pipe.ts
+++ src/types/pipe.ts
@@ -1,3 +1,91 @@
-export function pipe<T>(value: T, ...fns: ((input: T) => T)[]): T {
-  return fns.reduce((acc, fn) => fn(acc), value);
+type UnknownFunction = (...params: unknown[]) => unknown;
+
+export function pipe<A extends unknown[], B>(
+  ab: (...a: A) => B,
+): (...args: A) => B;
+
+export function pipe<A extends unknown[], B, C>(
+  ab: (...a: A) => B,
+  bc: (b: B) => C,
+): (...args: A) => C;
+
+export function pipe<A extends unknown[], B, C, D>(
+  ab: (...a: A) => B,
+  bc: (b: B) => C,
+  cd: (c: C) => D,
+): (...args: A) => D;
+
+export function pipe<A extends unknown[], B, C, D, E>(
+  ab: (...a: A) => B,
+  bc: (b: B) => C,
+  cd: (c: C) => D,
+  de: (d: D) => E,
+): (...args: A) => E;
+
+export function pipe<A extends unknown[], B, C, D, E, F>(
+  ab: (...a: A) => B,
+  bc: (b: B) => C,
+  cd: (c: C) => D,
+  de: (d: D) => E,
+  ef: (e: E) => F,
+): (...args: A) => F;
+
+export function pipe<A extends unknown[], B, C, D, E, F, G>(
+  ab: (...a: A) => B,
+  bc: (b: B) => C,
+  cd: (c: C) => D,
+  de: (d: D) => E,
+  ef: (e: E) => F,
+  fg: (f: F) => G,
+): (...args: A) => G;
+
+export function pipe(...fns: UnknownFunction[]): UnknownFunction {
+  return (...initialParams: unknown[]): unknown =>
+    fns.reduce<unknown>((value, fn, index) => {
+      const params = index === 0 ? (value as unknown[]) : [value];
+      return fn(...params);
+    }, initialParams);
+}
+
+export function pipeWith<A, B>(a: A, ab: (a: A) => B): B;
+
+export function pipeWith<A, B, C>(a: A, ab: (a: A) => B, bc: (b: B) => C): C;
+
+export function pipeWith<A, B, C, D>(
+  a: A,
+  ab: (a: A) => B,
+  bc: (b: B) => C,
+  cd: (c: C) => D,
+): D;
+
+export function pipeWith<A, B, C, D, E>(
+  a: A,
+  ab: (a: A) => B,
+  bc: (b: B) => C,
+  cd: (c: C) => D,
+  de: (d: D) => E,
+): E;
+
+export function pipeWith<A, B, C, D, E, F>(
+  a: A,
+  ab: (a: A) => B,
+  bc: (b: B) => C,
+  cd: (c: C) => D,
+  de: (d: D) => E,
+  ef: (e: E) => F,
+): F;
+
+export function pipeWith<A, B, C, D, E, F, G>(
+  a: A,
+  ab: (a: A) => B,
+  bc: (b: B) => C,
+  cd: (c: C) => D,
+  de: (d: D) => E,
+  ef: (e: E) => F,
+  fg: (f: F) => G,
+): G;
+
+export function pipeWith(value: unknown, ...fns: UnknownFunction[]): unknown {
+  // @ts-expect-error -- We're intentionally spreading the functions array for the pipe implementation.
+  return pipe(...fns)(value);
 }
